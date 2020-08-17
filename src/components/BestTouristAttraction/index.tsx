3090effae55897f424e476e0b086f5303056a60b
YoungWukJeon <kdh10080@gmail.com>
2020-08-17 23:47:35 +0900

    BEST 명소 component 추가

---
+++ src/components/BestTouristAttraction/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { IonCard, IonIcon, IonImg } from "@ionic/react";
+import { heart } from "ionicons/icons";
+
+import "./index.scss";
+
+interface BestTouristAttractionProps {
+  regionName: string;
+  recommendCount: number;
+  name: string;
+  imageUrl: string;
+}
+
+const BestTouristAttraction: React.FC<BestTouristAttractionProps> = ({
+  regionName,
+  recommendCount,
+  name,
+  imageUrl,
+}) => {
+  return (
+    <IonCard className="best-tourist-attraction">
+      <div className="item-area">
+        <span className="best-tourist-attraction-region-name">
+          {regionName}
+        </span>
+      </div>
+      <div className="item-area">
+        <IonIcon slot="start" icon={heart} className="heart-icon" />
+        <div className="best-tourist-attraction-recommend-count">
+          {recommendCount}
+        </div>
+      </div>
+      <div className="item-area">
+        <div className="best-tourist-attraction-name">{name}</div>
+      </div>
+      <IonImg src={imageUrl} />
+    </IonCard>
+  );
+};
+
+export default BestTouristAttraction;
---
+++ src/pages/tabs/Home/index.test.tsx
@@ -0,0 +1,24 @@
+import 'core-js';
+
+export const HomeTest = describe('HomeTest', () => {
+    test('1차원 배열 -> 2차원 배열 변환', () => {
+        const bestTouristAttractionInfos = [
+            { regionName: "괴산", recommendCount: 6, name: "괴산청결고추박물관", imageUrl: "/assets/images/best01.png" },
+            { regionName: "괴산", recommendCount: 3, name: "괴산한지체험박물관", imageUrl: "/assets/images/best02.png" },
+            { regionName: "보은", recommendCount: 2, name: "솔향공원", imageUrl: "/assets/images/best03.png" },
+            { regionName: "영동", recommendCount: 1, name: "난계국악기체험전수관", imageUrl: "/assets/images/best04.png" }
+        ];
+    
+        const bestTouristAttractions = [];
+        const originalArray = [...bestTouristAttractionInfos];
+
+        while (bestTouristAttractionInfos.length) {
+            bestTouristAttractions.push(bestTouristAttractionInfos.splice(0, 2));
+        }
+    
+        expect(bestTouristAttractions.length).toEqual(2);
+        expect(bestTouristAttractions[0].length).toEqual(2);
+        expect(bestTouristAttractions[1].length).toEqual(2);
+        expect(bestTouristAttractions.flat()).toEqual(originalArray);
+    });
+});
