6227260efcc1d272e5685e717a7606ffa9835d19
YoungWukJeon <kdh10080@gmail.com>
2020-08-18 05:00:15 +0900

    메인화면에 'BEST 명소' 추가

--- src/pages/tabs/Home/index.test.tsx
+++ src/pages/tabs/Home/index.test.tsx
@@ -1,6 +1,8 @@
 import 'core-js';
 
-export const HomeTest = describe('HomeTest', () => {
+import { convertArrayDimension } from './index';
+
+describe('HomeTest', () => {
     test('1차원 배열 -> 2차원 배열 변환', () => {
         const bestTouristAttractionInfos = [
             { regionName: "괴산", recommendCount: 6, name: "괴산청결고추박물관", imageUrl: "/assets/images/best01.png" },
@@ -9,16 +11,11 @@ export const HomeTest = describe('HomeTest', () => {
             { regionName: "영동", recommendCount: 1, name: "난계국악기체험전수관", imageUrl: "/assets/images/best04.png" }
         ];
     
-        const bestTouristAttractions = [];
-        const originalArray = [...bestTouristAttractionInfos];
+        const bestTouristAttractions = convertArrayDimension(bestTouristAttractionInfos);
 
-        while (bestTouristAttractionInfos.length) {
-            bestTouristAttractions.push(bestTouristAttractionInfos.splice(0, 2));
-        }
-    
         expect(bestTouristAttractions.length).toEqual(2);
         expect(bestTouristAttractions[0].length).toEqual(2);
         expect(bestTouristAttractions[1].length).toEqual(2);
-        expect(bestTouristAttractions.flat()).toEqual(originalArray);
+        expect(bestTouristAttractions.flat()).toEqual(bestTouristAttractionInfos);
     });
 });
