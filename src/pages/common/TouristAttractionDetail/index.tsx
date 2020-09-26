a5a849941b4dca6c6390d19b621c6bad4db2a2cd
YoungWukJeon <kdh10080@gmail.com>
2020-09-26 09:44:49 +0900

    명소 상세 정보에서 서버와 통신하기 위해 URL Params 구하기, 더미 데이터 생성

--- src/pages/common/TouristAttractionDetail/index.tsx
+++ src/pages/common/TouristAttractionDetail/index.tsx
@@ -1,15 +1,44 @@
-import React, {useRef} from "react";
+import React from "react";
 import {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar, useIonViewWillEnter
 } from "@ionic/react";
 
 import "./index.scss";
+import {RouteComponentProps} from "react-router";
 
-const TouristAttractionDetail: React.FC = () => {
+const touristAttractionInfo = {
+    no: 1,
+    name: "괴산 문화원",
+    imageUrl: ["/assets/images/best01.png", "/assets/images/best02/png", "/assets/images/best03.png"],
+    address: "충북 괴산군 괴산읍 읍내로 268",
+    latitude: 36.807167849,
+    longitude: 127.7937187832,
+    phone: '괴산문화원 043-832-3588',
+    intro: '괴산문화원은 지역문화축제와 지역사회발전을 위한 문화활동을 전개해 가는 문화공간이다.',
+    time: '09:00~18:00<br />※ 강좌에 따라 변동 가능',
+    cost: null,
+    kind: 'culture',
+    sharedCount: 2,
+    recommendCount: 0
+};
+
+interface MatchParams {
+    id: string
+}
+
+const TouristAttractionDetail: React.FC<RouteComponentProps<MatchParams>> = ({ match }) => {
+    const fetchData = () => {
+        console.log(match.params.id);
+    };
+
+    useIonViewWillEnter(() => {
+        fetchData();
+    });
+
     return (
         <IonPage>
             <IonHeader>
@@ -24,9 +53,9 @@ const TouristAttractionDetail: React.FC = () => {
                 {/*    </IonToolbar>*/}
                 {/*</IonHeader>*/}
 
-                ㅎㅇ
+                <div>
 
-
+                </div>
             </IonContent>
         </IonPage>
     );
